3dc7e84c7500af84abb782c80757039f1609fa38
nezcal <darkcloudfleethoof@gmail.com>
2017-04-20 11:26:12 -0400

    Added arcana card image, attempted to get it to render unsuccessfully

--- src/main/java/nezcal/persona/ItemArcanaCard.java
+++ src/main/java/nezcal/persona/ItemArcanaCard.java
@@ -1,8 +1,12 @@
 package nezcal.persona;
 
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Created by nezcal on 4/19/2017 at 11:43 PM.
@@ -12,5 +16,10 @@ public class ItemArcanaCard extends Item {
         setRegistryName("itemArcanaCard");        // The unique name (within your mod) that identifies this item
         setUnlocalizedName(main.MODID + ".itemArcanaCard");     // Used for localization (en_US.lang)
         GameRegistry.register(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
